d6e28c92aea3ebcdd00b181fc8fc78fe03223471
MarieLvsq <maarielevesque@gmail.com>
2023-10-07 15:48:32 +0200

    premier commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Message from "./Message";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
+        My button
+      </Button>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
+    </div>
+  );
+}
+
+export default App;
